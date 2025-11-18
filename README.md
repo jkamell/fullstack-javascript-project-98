4a7ddc127b3ba7c675041677e3c0d6a88f64fd92
Jkamell <javierkamellyaspe@yahoo.com>
2025-11-17 19:54:54 -0500

    Se programa juego de calculadora, se juega y se graba en asciinema

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 
 [![Maintainability](https://qlty.sh/gh/jkamell/projects/fullstack-javascript-project-98/maintainability.svg)](https://qlty.sh/gh/jkamell/projects/fullstack-javascript-project-98)
 
-### video asciinema
-https://asciinema.org/a/3Jf0vjuz0A1vuNnxaTu3Dv1dM
+### video asciinema paso-5
+[![Demo](https://asciinema.org/a/3Jf0vjuz0A1vuNnxaTu3Dv1dM.svg)](https://asciinema.org/a/3Jf0vjuz0A1vuNnxaTu3Dv1dM)
+
+
+### video asciinema paso-6
+[![Demo](https://asciinema.org/a/Ro9A4A5454hd2xZZcsjefJ1w9.svg)](https://asciinema.org/a/Ro9A4A5454hd2xZZcsjefJ1w9)
---
+++ src/games/calc.js
@@ -0,0 +1,35 @@
+import runEngine from '../index.js';
+
+const description = 'Resuelve la operación matemática. Responde con el número correcto.';
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const operators = ['+', '-', '*'];
+
+const calculate = (a, b, operator) => {
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    default:
+      return null;
+  }
+};
+
+const makeRound = () => {
+  const num1 = getRandomNumber(1, 25);
+  const num2 = getRandomNumber(1, 25);
+  const operator = operators[getRandomNumber(0, operators.length - 1)];
+
+  const question = `${num1} ${operator} ${num2}`;
+  const correctAnswer = calculate(num1, num2, operator).toString();
+
+  return [question, correctAnswer];
+};
+
+const runCalcGame = () => runEngine(description, makeRound);
+
+export default runCalcGame;
